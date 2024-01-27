9de4df28871e00504f5ed2858d6dfac6c11118cc
Paulius Jakonis <paulius.jakonis@gmail.com>
2024-01-27 11:17:37 +0200

    Pridetas bootstrap 5 ir pridėtas "headeris"

--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Employee
 
 # Create your views here.
 def index (request):
-    return render(request,'employees/index.html')
+    return render(request,'employees/index.html', {
+        'employees': Employee.objects.all()
+    })
 
