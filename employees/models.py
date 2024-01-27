64df482c919928f15743184d0e27a053d6237395
Paulius Jakonis <paulius.jakonis@gmail.com>
2024-01-27 10:29:46 +0200

    Aprašytas darbuotojo modelis models.py file

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    employee_number = models.PositiveIntegerField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    department = models.CharField(max_length=30)
+    position = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+    adress = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'Student: {self.first_name} {self.last_name}'
